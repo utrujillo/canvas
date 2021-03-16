1e8dc0e4fef1ee52788396334e7bce2491a07db0
Uziel Trujillo Colon <uziel.trujillo@gmail.com>
2021-03-16 14:22:10 -0600

    Ejercicio lineas

---
+++ ejercicios/lineas/js/lineas.js
@@ -0,0 +1,18 @@
+let canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
+
+context.beginPath();
+context.strokeStyle = '#FF0000';
+context.moveTo(300, 250);
+context.lineTo(100, 200);
+context.lineWidth = 10;
+context.stroke();
+context.closePath();
+
+context.beginPath();
+context.strokeStyle = '#0000FF';
+context.moveTo(10, 100);
+context.lineTo(50, 100);
+context.lineWidth = 5;
+context.stroke();
+context.closePath();
