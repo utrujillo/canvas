8adb1f71adc590b907b91b33143066f94d3b81a1
Uziel Trujillo Colon <uziel.trujillo@gmail.com>
2021-03-17 14:44:40 -0600

    Practica de rectangulos

--- ejercicios/lineas/js/lineas.js
+++ ejercicios/lineas/js/lineas.js
@@ -1,18 +1,19 @@
 let canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d');
 
-context.beginPath();
-context.strokeStyle = '#FF0000';
-context.moveTo(300, 250);
-context.lineTo(100, 200);
-context.lineWidth = 10;
-context.stroke();
-context.closePath();
+// context.beginPath();
+// context.strokeStyle = '#FF0000';
+// context.moveTo(300, 250);
+// context.lineTo(100, 200);
+// context.lineWidth = 10;
+// context.stroke();
+// context.closePath();
 
 context.beginPath();
 context.strokeStyle = '#0000FF';
-context.moveTo(10, 100);
+context.moveTo(300, 100);
 context.lineTo(50, 100);
+context.lineTo(25, 50);
 context.lineWidth = 5;
 context.stroke();
 context.closePath();
