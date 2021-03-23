02eec0b57a4bcdeed463aaed35e30e5d93e77cd4
Uziel Trujillo Colon <uziel.trujillo@gmail.com>
2021-03-23 14:12:29 -0600

    Interface grafica

---
+++ ejercicios/traslacion/js/core.js
@@ -0,0 +1,11 @@
+let canvas = document.getElementById('canvas'),
+  context = canvas.getContext('2d')
+
+context.fillStyle = '#FF0000'
+//               x,  y , alto, ancho
+// x' = x + d
+// y' = y + d
+context.fillRect( 20, 20, 30, 30 );
+
+
+
