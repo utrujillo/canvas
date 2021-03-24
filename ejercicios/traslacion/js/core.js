2c4731bd1a6e10904bc57b94fd45bec5296bce9c
Uziel Trujillo Colon <uziel.trujillo@gmail.com>
2021-03-24 15:02:27 -0600

    Agregando la animacion

--- ejercicios/traslacion/js/core.js
+++ ejercicios/traslacion/js/core.js
@@ -1,11 +1,54 @@
 let canvas = document.getElementById('canvas'),
-  context = canvas.getContext('2d')
+  context = canvas.getContext('2d'),
+  btnFijo = document.getElementById('btnFijo'),
+  btnReload = document.getElementById('btnReload'),
+  btnTrasladar = document.getElementById('btnTrasladar'),
+  posX = document.getElementById('posX'),
+  posY = document.getElementById('posY'),
+  btnAnimate = document.getElementById('btnAnimate'),
+  posInitX = 20,
+  posInitY = 20
 
 context.fillStyle = '#FF0000'
 //               x,  y , alto, ancho
 // x' = x + d
 // y' = y + d
-context.fillRect( 20, 20, 30, 30 );
+context.fillRect( posInitX, posInitY, 30, 30 )
+
+btnFijo.addEventListener( 'click', function(){
+  cleanCanvas()
+  let x_distancia = 200
+  context.fillRect( posInitX + x_distancia, 20, 30, 30 )
+} )
+
+btnReload.addEventListener( 'click', function (){
+  location.reload();
+} )
+
+btnTrasladar.addEventListener( 'click', function (){
+  // console.log(` PosX: ${posX.value} PosY: ${posY.value} `)
+  cleanCanvas()
+  context.fillRect( posX.value, posY.value, 30, 30 )
+} )
+
+btnAnimate.addEventListener( 'click', slowMotion )
+
+function slowMotion(){
+  setTimeout( function(){
+    posInitX += 1
+    posInitY += 1
+    console.log( `posInitX: ${posInitX}  posInitY: ${posInitY}` )
+    cleanCanvas()
+    context.fillRect( posInitX, posInitY, 30, 30 )
+    slowMotion()
+  }, 1 )
+}
+
+function cleanCanvas() {
+  context.clearRect( 0, 0, canvas.width, canvas.height )
+}
+
+
 
 
 
